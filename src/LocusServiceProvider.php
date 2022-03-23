27d02bd280fb8e6eb589f39ea5942554f31a22c9
Pierre-Glanum <pierre@glanum.com>
2022-03-23 17:24:30 +0100

    feat: (wip) poc on dynamic locales and ulr translation

--- src/LocusServiceProvider.php
+++ src/LocusServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace Glanum\Locus;
 
+use Illuminate\Routing\Router;
+use Illuminate\Routing\RouteRegistrar;
+use Illuminate\Support\Str;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Glanum\Locus\Commands\LocusCommand;
@@ -20,5 +23,28 @@ class LocusServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasViews()
             ->hasCommand(LocusCommand::class);
+    }
+
+    public function packageBooted()
+    {
+        Router::macro('localize', function ($callback) {
+            if (true) {
+                $callback();
+            }
+
+            foreach (['en', 'fr'] as $locale) {
+                $routeRegistar = (new RouteRegistrar($this))->attribute('prefix', $locale);
+
+                $routeRegistar->group(function () use ($callback) {
+                    $callback();
+                });
+
+
+            }
+
+            foreach ($this->getRoutes()->getRoutes() as $route) {
+                $route->setUri(Str::of($route->uri())->replace('product', 'produit'));
+            }
+        });
     }
 }
